d7b5927578c1bcdc717003ca176304c08b8e105f
Belloshehu <belloshehu1@gmail.com>
2022-07-05 22:53:36 +0100

    Add configuration for development and Production.

--- errand/settings.py
+++ errand/settings.py
@@ -11,26 +11,28 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from configurations import Configuration, values
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '09onf5dn3qp+r6urhm^q*k1*+c8%+zmq&1%7!+nm48adx$mq)w'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+class Dev(Configuration):
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Application definition
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-INSTALLED_APPS = [
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '09onf5dn3qp+r6urhm^q*k1*+c8%+zmq&1%7!+nm48adx$mq)w'
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = values.BooleanValue(True)
+
+    ALLOWED_HOSTS = values.ListValue(['localhost', '0.0.0.0',])
+
+
+    # Application definition
+
+    INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,9 +44,9 @@ INSTALLED_APPS = [
     'sharing',
     'reviews',
     'friendship',
-]
+    ]
 
-MIDDLEWARE = [
+    MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,11 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    ]
 
-ROOT_URLCONF = 'errand.urls'
+    ROOT_URLCONF = 'errand.urls'
 
-TEMPLATES = [
+    TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
@@ -70,26 +72,26 @@ TEMPLATES = [
             ],
         },
     },
-]
+    ]
 
-WSGI_APPLICATION = 'errand.wsgi.application'
+    WSGI_APPLICATION = 'errand.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+    # Database
+    # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+    DATABASES = values.DatabaseURLValue(f"sqlite:///{BASE_DIR}/db.sqlite3")
 
+    # Password validation
+    # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
+    AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -102,33 +104,41 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
+    ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = values.Value('UTC')
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDERECT_URL = '/accounts/profile'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+    LOGIN_REDERECT_URL = '/accounts/profile'
+    LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-AUTH_USER_MODEL = 'account.User'
+    AUTH_USER_MODEL = 'account.User'
+
+
+class Prod(Dev):
+    """
+    Configuration for production.
+    """
+    DEBUG = values.BooleanValue(False)
+    SECRET_KEY = values.SecretValue()
