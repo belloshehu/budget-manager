aa8c422f3ab409e16b1d786b1ca51889da087c1a
Belloshehu <belloshehu1@gmail.com>
2022-08-04 11:24:19 +0100

    Fix EMAIL_HOST_USER value in settings.py

--- errand/settings.py
+++ errand/settings.py
@@ -174,7 +174,7 @@ class Dev(Configuration):
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = belloshehu1@gmail.com>
+    EMAIL_HOST_USER = "belloshehu1@gmail.com"
     EMAIL_HOST_PASSWORD = "vrzjtkgkfpmpyxws"
 
     
